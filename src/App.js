b6e53fca53ff27ce16d1853a0b70da10f3e00f5c
Noor <noormir1295@gmail.com>
2020-11-10 16:58:47 -0500

    adding intro nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from "react";
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
+  return ( 
+    <div style={{ height: "300px", position: "relative" }}>
+      <Layout
+        style={{
+          background:
+            "url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover",
+        }}
+      >
+        <Header transparent title="Title" style={{ color: "white" }}>
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">About Me</a>
+            <a href="/">Resume</a>
+            <a href="/">Projects</a>
+            <a href="/">Contact</a>
+          </Navigation>
+        </Drawer>
+        <Content > <h1 classname="">Noor Mir</h1> </Content>
+      </Layout>
     </div>
   );
 }
