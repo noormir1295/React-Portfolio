5b99200328afe054337d34c4f0b4ac0f5cd182b6
Noor <noormir1295@gmail.com>
2020-11-10 17:53:53 -0500

    adding Home page and Header Component

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Container, Row } from "reactstrap";
+import { Container } from "reactstrap";
 
 import "./style.css";
 import Header from "../../components/Header";
 
 
-const home = () => {
+const Home = () => {
     return (
         <div>
             <Container>
                 <Header/>
+                <section id='home' className="flex height-fix">
+                    <div id='pt' className='canvas'></div>
+                    <div className="flex">
+                        <div className="text">
+                            Hello, I am <span className="highlight">Noor Mir</span>
+                            <br/>
+                            I am a graduated student from the University of Miami and is looking to be a Web Developer.
+                            <br/>
+                            <br/>
+                            <p>Thank you for taking the time out to visit my Website</p>
+                        </div>
+                    </div>
+                    {/* Footer goes here */}
+                </section>
             </Container>
         </div>
     )
 }
+export default Home
