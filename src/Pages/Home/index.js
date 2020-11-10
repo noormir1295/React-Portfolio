dc57e22f64fed80945962bbc7ea35b26b45c89e9
Noor <noormir1295@gmail.com>
2020-11-10 17:47:46 -0500

    restarting by changing files

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row } from "reactstrap";
+
+import "./style.css";
+import Header from "../../components/Header";
+
+
+const home = () => {
+    return (
+        <div>
+            <Container>
+                <Header/>
+            </Container>
+        </div>
+    )
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavbarBrand,
+} from "reactstrap";
+
+import "./style.css";
+
+const Header = (props) => {
+    const [isOpen, setIsOpen] = useState(props);
+
+    return (
+      <>
+        <Navbar expand="md" fixed="top">
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto-navbar justify-content-center fixed-top header">
+              <NavItem>
+                <Link to="/home" className="btn m-2 btn success">
+                  HOME
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/about" className="btn m-2 btn success">
+                  ABOUT
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/portfolio" className="btn m-2 btn success">
+                  PORTFOLIO
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/resume" className="btn m-2 btn success">
+                  RESUME
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/contact" className="btn m-2 btn success">
+                  CONTACT
+                </Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </>
+    );
+}
+
+export default Header;
